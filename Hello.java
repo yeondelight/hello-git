bc5028b84c63d8b99bbc52a5ccb33defe1879d92
dykim <hihi5v@hansung.ac.kr>
2020-11-03 13:41:53 +0900

    Fourth commit

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello, world from GIT!");
 		System.our.println("Changes after first commit!");
+        System.out.println("Changes after Pushing to remote!");
 	}
 }
